77683bdf29dfef4132132360d626e3c048025b5b
dancorrea86 <danielmacielcorrea@gmail.com>
2019-09-18 22:05:12 -0300

    second commit

---
+++ src/Input.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function Input() {
+  return(
+    <header>
+      <input type="text"/>
+      <button>Ok</button>
+    </header>
+  )
+}
+
+export default Input;
